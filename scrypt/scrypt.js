d0635b588ceed12d7664ebf768b235ddb141b671
chenpetro <chenpetro@gmail.com>
2025-02-23 21:31:14 +0100

    explore section done

--- scrypt/scrypt.js
+++ scrypt/scrypt.js
@@ -77,6 +77,105 @@ window.onload = function() {
 
 
 
+// Explore section ////////////////////////////////////
+
+exploreToggleBtn = document.querySelector('.explore__toggle__btn')
+exploreToggle = document.querySelector('.explore__toggle__btn__circle') 
+exploreToggleMin = document.querySelector('.explore__toggle__min')
+exploreToggleRecom = document.querySelector('.explore__toggle__recom')
+exploreCPU = document.querySelector('.explore__box__desc__CPU')
+exploreRAM = document.querySelector('.explore__box__desc__RAM')
+exploreOS = document.querySelector('.explore__box__desc__OS')
+exploreVideo = document.querySelector('.explore__box__desc__video')
+explorePixel = document.querySelector('.explore__box__desc__pixel')
+exploreVertex = document.querySelector('.explore__box__desc__vertex')
+exploreSpace = document.querySelector('.explore__box__desc__space')
+explorevRAM = document.querySelector('.explore__box__desc__vRAM')
+
+const exploreOriginalToggle = exploreToggle.style.marginLeft
+const exploreOriginalToggleMin = exploreToggleMin.style.color
+const exploreOriginalToggleRecom = exploreToggleRecom.style.color
+
+const CPUOriginalText = exploreCPU.textContent;
+const RAMOriginalText = exploreRAM.textContent;
+const OSOriginalText = exploreOS.textContent;
+const VideoOriginalText = exploreVideo.textContent;
+const vRAMOriginalText = explorevRAM.textContent;
+
+let isToggled = false;
+
+exploreToggleBtn.addEventListener('click', ()=>{
+    if (isToggled) {
+        // If currently toggled, revert to original text
+        exploreCPU.textContent = CPUOriginalText;
+        exploreRAM.textContent = RAMOriginalText;
+        exploreOS.textContent = OSOriginalText;
+        exploreVideo.textContent = VideoOriginalText;
+        explorevRAM.textContent = vRAMOriginalText;
+        exploreToggle.style.marginLeft = exploreOriginalToggle;
+        exploreToggleMin.style.color = exploreOriginalToggleMin
+        exploreToggleRecom.style.color = exploreOriginalToggleRecom
+    } else {
+        // If not toggled, change to new text
+        exploreCPU.textContent = 'Intel i5-3200k or AMD Ryzen 5 1200';
+        exploreRAM.textContent = '16 GB';
+        exploreOS.textContent = 'Windows 11 64-bit';
+        exploreVideo.textContent = 'NVIDIA GTX 1060 (6 GB) or AMD RX 570 (4 GB)';
+        explorevRAM.textContent = '6 GB';
+        exploreToggle.style.marginLeft = '20px';
+        exploreToggleMin.style.color = '#30373f';
+        exploreToggleRecom.style.color = 'white';
+    }
+    // Flip the toggle state
+    isToggled = !isToggled;
+})
+
+//Explore Section - part 2 //////////////////////////////////////////
+
+exploreToggle2Btn = document.querySelector('.explore__toggle2__btn')
+exploreToggle2 = document.querySelector('.explore__toggle2__btn__circle') 
+exploreToggle2Stand = document.querySelector('.explore__toggle2__stand')
+exploreToggle2Limit = document.querySelector('.explore__toggle2__limit')
+explorePlatform = document.querySelector('.explore__box2__desc__platform')
+explorePremiere = document.querySelector('.explore__box2__desc__premiere')
+explorePublisher = document.querySelector('.explore__box2__desc__publisher')
+exploreSpecies = document.querySelector('.explore__box2__desc__species')
+exploreVoice = document.querySelector('.explore__box2__desc__voice')
+exploreLang = document.querySelector('.explore__box2__desc__lang')
+
+const exploreOriginalToggle2 = exploreToggle2.style.marginLeft
+const exploreOriginalToggle2Stand = exploreToggle2Stand.style.color
+const exploreOriginalToggle2Limit = exploreToggle2Limit.style.color
+
+const PlatformOriginalText = explorePlatform.textContent;
+const VoiceOriginalText = exploreVoice.textContent;
+const LangOriginalText = exploreLang.textContent;
+
+
+let isToggled2 = false;
+
+exploreToggle2Btn.addEventListener('click', ()=>{
+    if (isToggled2) {
+        // If currently toggled, revert to original text
+        explorePlatform.textContent = PlatformOriginalText;
+        exploreVoice.textContent = VoiceOriginalText;
+        exploreLang.textContent = LangOriginalText;
+        exploreToggle2.style.marginLeft = exploreOriginalToggle2;
+        exploreToggle2Stand.style.color = exploreOriginalToggle2Stand
+        exploreToggle2Limit.style.color = exploreOriginalToggle2Limit
+    } else {
+        // If not toggled, change to new text
+        explorePlatform.textContent = 'PS4, PS5';
+        exploreVoice.textContent = 'English, German, Ukrainian';
+        exploreLang.textContent = 'English, German, Polish, Dutch, Ukranian';
+        exploreToggle2.style.marginLeft = '20px';
+        exploreToggle2Stand.style.color = '#30373f';
+        exploreToggle2Limit.style.color = 'white';
+    }
+    // Flip the toggle state
+    isToggled2 = !isToggled2;
+})
+
 
 
 
