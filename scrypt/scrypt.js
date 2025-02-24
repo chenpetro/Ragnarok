7aee2815f056da23caaf195a9cd82ab8740d8626
chenpetro <chenpetro@gmail.com>
2025-02-24 20:55:17 +0100

    News section done(swiper)

--- scrypt/scrypt.js
+++ scrypt/scrypt.js
@@ -186,24 +186,25 @@ exploreToggle2Btn.addEventListener('click', ()=>{
 
 //Swiper/////////////////////////////////////////////////////////
 
-// const swiper = new Swiper('.swiper', {
-//     // Optional parameters
-//     direction: 'vertical',
-//     loop: true,
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    //   clickable: true,
+    // },
   
-//     // If we need pagination
-//     pagination: {
-//       el: '.swiper-pagination',
-//     },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   
-//     // Navigation arrows
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-  
-//     // And if we need scrollbar
-//     scrollbar: {
-//       el: '.swiper-scrollbar',
-//     },
-//   });
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
